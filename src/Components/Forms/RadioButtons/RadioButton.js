4328e4c3a58f552d465b0b70cb228ed8ae83a10b
Your Name <btrangcal@gmail.com>
2019-03-27 22:12:46 -0700

    fixed prop duplication

--- src/Components/Forms/RadioButtons/RadioButton.js
+++ src/Components/Forms/RadioButtons/RadioButton.js
@@ -17,14 +17,13 @@ export const RadioButton=(props)=>{
         <div className={`Radio__choice `}>
             <div className={chosenClass}>
                 <input
-                    className="Radio__choice--input" 
+                    className={`Radio__choice--input ${chosenClass}`}
                     type="radio" 
                     id={props.description} 
                     name={props.description} 
                     value={props.value} 
                     onChange={(e)=>{props.changed(e)}}
-                    checked={props.checked}
-                    className={chosenClass}/>
+                    checked={props.checked}/>
                 <label htmlFor={props.description}>{props.description}</label>
             </div>
         </div>

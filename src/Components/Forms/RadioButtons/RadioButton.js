71d00c06a4881d6d019ec4a9ddcb40fc80e01943
Your Name <btrangcal@gmail.com>
2019-03-27 23:04:05 -0700

    radio wrapper responsive

--- src/Components/Forms/RadioButtons/RadioButton.js
+++ src/Components/Forms/RadioButtons/RadioButton.js
@@ -15,17 +15,16 @@ export const RadioButton=(props)=>{
 
     return(
         <div className={`Radio__choice `}>
-            <div className={chosenClass}>
                 <input
-                    className={`Radio__choice--input ${chosenClass}`}
+                    className={`Radio__choice--input`}
                     type="radio" 
                     id={props.description} 
                     name={props.description} 
                     value={props.value} 
                     onChange={(e)=>{props.changed(e)}}
-                    checked={props.checked}/>
+                    checked={props.checked}
+                />
                 <label htmlFor={props.description}>{props.description}</label>
-            </div>
         </div>
         );
     

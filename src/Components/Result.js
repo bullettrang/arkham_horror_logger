834dd9a14a45bb482610e6bc23768b5347655dda
Your Name <btrangcal@gmail.com>
2019-04-29 16:53:29 -0700

    added tabs, current File fetching

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,49 +1,81 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import ResultImage from './ResultImage';
 import {SCENARIO_IMGS} from  '../constants/ScenarioImages';
 import {RESOLUTIONS} from '../constants/results';
 import * as actions from '../actions/index';
 import './Result.css'
+import './react-tabs.css';
 
 
-const ResultPicture=(props)=>{
-    const bgStyled={
-        background:`url(${props.picture})`,
-        backgroundSize:'cover',
-        backgroundPosition:"center",
-        maxWidth:'250px',
-        minWidth:'100px',
-        maxHeight:'auto',
-        minHeight:'100px',
-        marginRight:'2em',
-        borderRadius:'3px',
-    }
-    return(
-        <div className="Result-Picture" style={{...bgStyled}}>
-
-        </div>
-    )
-}
 class Result extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={
+            scenarios:[],
+            toDashBoard:false
+        }
+    }
+
+
     componentDidMount(){
-        //get results depending on user's scenario
-        //calculate percentages of each question
-        //display ratio of user's pick
-        //display ratio of other users' picks
-        if(this.props.currentFile!==null){
+
+        this.props.fetchFiles();                //refresh user files
+        if(this.props.currentFile!==null){  
             console.log(this.props.currentFile);
             const userScenarios = this.props.currentFile.completedScenarios.map(e=> e.scenarioTitle);
             console.log(userScenarios);
+
+            this.setState({scenarios:userScenarios});
             this.props.fetchResults(userScenarios[0]);
+        }
+        else{
+            this.setState({toDashBoard:true});
         }
 
     }
 
+    renderTabs=()=>{
+       return this.state.scenarios.map((sc)=>{
+            return(
+                <Tab key={sc}>
+                    {sc}
+                </Tab>
+            );
+        });
+    }
+
+    renderTabsPanels=()=>{
+       return this.state.scenarios.map((sc)=>{
+            return(
+                <TabPanel key={sc}>
+                    <div className="Result-Content">
+                        <ResultImage
+                            picture={SCENARIO_IMGS[sc]}
+                        />
+                        <div>
+                            You chose to save the students
+                        </div>
+                        <div>
+                            55%
+                        </div>
+                    </div>
+                </TabPanel>
+            );
+        });
+    }
+
     render(){
+        const {toDashBoard}= this.state;
+        if(toDashBoard){
+            return <Redirect to="/"/>
+        }
         return(
             <div className="Result-Wrapper">
-                <div className="Result-Content">
+                {/* <div className="Result-Content">
                     <ResultPicture 
                         picture={SCENARIO_IMGS['The Gathering']}
                     />
@@ -64,9 +96,16 @@ class Result extends Component{
                     <div>
                         55%
                     </div>
-                </div>
+                </div> */}
+            <Tabs>
+                <TabList>
+                    {this.renderTabs()}
+                </TabList>
+                {this.renderTabsPanels()}
+            </Tabs> 
             </div>
-        )
+
+        );
 
     }
 
---
+++ src/Components/ResultImage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {SCENARIO_IMGS} from  '../constants/ScenarioImages';
+const ResultImage=(props)=>{
+    const bgStyled={
+        background:`url(${props.picture})`,
+        backgroundSize:'cover',
+        backgroundPosition:"center",
+        maxWidth:'250px',
+        minWidth:'100px',
+        maxHeight:'auto',
+        minHeight:'100px',
+        marginRight:'2em',
+        borderRadius:'3px',
+    }
+    return(
+        <div className="Result-Picture" style={{...bgStyled}}>
+
+        </div>
+    )
+}
+
+export default ResultImage;

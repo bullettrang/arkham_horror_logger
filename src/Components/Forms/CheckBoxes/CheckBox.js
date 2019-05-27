8e070e143ec864416fe30e3d89dae2dcd3b94cec
Your Name <btrangcal@gmail.com>
2019-05-26 19:23:01 -0700

    adjusted form padding

--- src/Components/Forms/CheckBoxes/CheckBox.js
+++ src/Components/Forms/CheckBoxes/CheckBox.js
@@ -1,16 +1,8 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import EldersignSvg from '../../DashBoard/SVGs/EldersignSvg';
 import './CheckBox.css';
 
-const animateElder = {
-  animation: 'dash 2.5s linear forwards'
-}
-
-const removeElder={
-  animation:'remove 2.5s linear forwards'
-}
 
 export const CheckBox=({type= 'checkbox',description,checked=false,onChange})=>{
   return(
@@ -23,20 +15,11 @@ export const CheckBox=({type= 'checkbox',description,checked=false,onChange})=>{
           value={description} 
           onChange={e=>onChange(e)}
     />
-  <span className="check-toggle"><EldersignSvg style={ checked ? animateElder:removeElder}width={'15px'} height={'15px'} /></span>
+  <span className="check-toggle"></span>
 </label>
   )
 }
 
-// export const CheckBox = ({ type = 'checkbox', description, checked = false, onChange }) => (
-//     <input 
-//         type={type} 
-//         name={description} 
-//         checked={checked} 
-//         value={description}
-//         onChange={(e)=>onChange(e)} 
-//     />
-// );
 
 CheckBox.propTypes = {
   type: PropTypes.string,
